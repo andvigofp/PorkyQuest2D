f8b2590f02d9de5e6b0ff94ac2620bc274862b3a
andvigofp <aniamcion96@gmail.com>
2026-03-11 17:07:37 +0100

    Añadido el barril, cuando personaje salte encima del barril salte efecto de la destruccion del barril

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,13 +21,14 @@ public class Player : MonoBehaviour
 
     private float move;
 
+ //Para que arranque los componentes de rigiBody 2d y la animacion
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();    
     }
 
-   
+   //Para que el personaje se pueda mover, las animaciones del perosnaje
     void Update()
     {
         move = Input.GetAxisRaw("Horizontal");
@@ -45,11 +46,13 @@ public class Player : MonoBehaviour
         animator.SetBool("IsGrounded", isGrounted);    
     }
 
+    //Para saber si el personaje toca el suele se active el check
     private void FixedUpdate()
     {
         isGrounted = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 
+    //Cuando el personaje recoge la moneda se destruye y se va sumando el texto
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.transform.CompareTag("Coin"))
@@ -59,9 +62,35 @@ public class Player : MonoBehaviour
             textCoints.text = coints.ToString();
         }
 
+        //Al pinchar el personaje en los pinchos o se cae por un barranco se reincia el nivel
         if(collision.transform.CompareTag("Spikes"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-    }
+
+        if(collision.transform.CompareTag("Barrel"))
+        {   //Para saber en que direccion queremos mandar al personaje    
+            Vector2 knockbackDir = (rb2D.position-(Vector2)collision.transform.position).normalized;
+        }
+
+        if(collision.transform.CompareTag("Barrel"))
+        {   //Para saber en que direccion queremos mandar al personaje    
+            Vector2 knockbackDir = (rb2D.position-(Vector2)collision.transform.position).normalized;
+            //Para reinciar un poco la velocidad
+            rb2D.linearVelocity = Vector2.zero;
+            rb2D.AddForce(knockbackDir*3, ForceMode2D.Impulse);
+
+            //Recorremos todos los boxCollider del barril 
+            BoxCollider2D[] colliders = collision.gameObject.GetComponents<BoxCollider2D>();
+
+            foreach(BoxCollider2D col in colliders)
+            {   
+                col.enabled = false;
+            }
+
+            collision.GetComponent<Animator>().enabled=true;
+            Destroy(collision.gameObject, 0.5f);
+        }
 }
+}
+
